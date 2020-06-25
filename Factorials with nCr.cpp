8c4aa69c9c7dffcb91e0f7a0c2909c6b5202cb1d
abhinavpal404 <44479397+abhinavpal404@users.noreply.github.com>
2020-06-25 17:41:38 +0530

    Update Factorials with nCr.cpp

--- Factorials with nCr.cpp
+++ Factorials with nCr.cpp
@@ -1,47 +1,62 @@
-int fact[N], invfact[N];
+const int e = 100005;
+const ll M = 1e9+7;
 
-int pow(int a, int b, int m)
+vector<ll>fact;
+vector<ll>invfact;
+
+template<typename T>void mod(T& a , T b)
 {
-	int ans=1;
-	while(b)
-	{
-		if(b&1)
-			ans=(ans*a)%m;
-		b/=2;
-		a=(a*a)%m;
-	}
-	return ans;
+    a = (a%b+b)%b;
 }
 
-int modinv(int k)
+template<typename T> T mpow(T a , T b)
 {
-	return pow(k, MOD-2, MOD);
+    T ans = 1;
+    while(b)
+    {
+        if(b&1)
+        ans = ans*a;
+        mod(ans,(T)M);
+        a = a*a;
+        mod(a,(T)M);
+        b>>=1;
+    }
+    mod(ans,(T)M);
+    return ans;
 }
 
-void precompute()
+
+template<typename T> T modinv(T a)
 {
-	fact[0]=fact[1]=1;
-	for(int i=2;i<N;i++)
-	{
-		fact[i]=fact[i-1]*i;
-		fact[i]%=MOD;
-	}
-	invfact[N-1]=modinv(fact[N-1]);
-	for(int i=N-2;i>=0;i--)
-	{
-		invfact[i]=invfact[i+1]*(i+1);
-		invfact[i]%=MOD;
-	}
+    return mpow(a,(T)M-2);
 }
 
-int nCr(int x, int y)
+
+void factorial()
 {
-	if(y>x)
-		return 0;
-	int num=fact[x];
-	num*=invfact[y];
-	num%=MOD;
-	num*=invfact[x-y];
-	num%=MOD;
-	return num;
+    fact.resize(e);
+    invfact.resize(e);
+    fact[0] = 1;
+    invfact[0] = 1;
+    loop(i,1,e)
+    {
+        fact[i] = fact[i-1]*i;
+        mod(fact[i],M);
+    }
+    loop(i,1,e)
+    {
+        invfact[i] = modinv(fact[i]);
+        mod(invfact[i],M);
+    }
+}
+
+
+ll ncr(ll n , ll r)
+{
+    ll ans ;
+    ans = fact[n]*invfact[n-r];
+    mod(ans,M);
+    ans = ans*invfact[r];
+    mod(ans,M);
+    return ans;
 }
